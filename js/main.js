fde5d2b07991106bd83110486eca24244447c2fe
Xamzaev-Xumoyun <xamzaevxumoyun49@gmail.com>
2022-04-01 20:56:56 +0500

    Add button boolkmark

--- js/main.js
+++ js/main.js
@@ -1,4 +1,9 @@
 var elList = document.querySelector(".list");
+var elSecondList = document.querySelector(".second-list");
+
+let elInput = document.querySelector(".form__input");
+
+let NewBtnArray =[];
 
 
 
@@ -32,33 +37,38 @@ function renderFilms(arr, element){
     var newImg = document.createElement("img");
     var newHeading = document.createElement("h3");
     var newText = document.createElement("p");
-    var newTime = document.createElement("time");
-    var newSubList = document.createElement("ul");
+    var newBtn = document.createElement("button");
+    // var newTime = document.createElement("time");
+    // var newSubList = document.createElement("ul");
 
 
     newHeading.textContent = film.title;
-    newText.textContent = film.overview.split(" ").slice(0 ,10).join(" ") + "...";
-    newTime.textContent = dateFormat(film.release_date);
+    newText.textContent = film.overview.split(" ").slice(0 ,16).join(" ") + "...";
+    // newTime.textContent = dateFormat(film.release_date);
+    newBtn.textContent = "Bookmark";
 
-    for(var genre of film.genres){
-      var newSubItem = document.createElement("li");
-      newSubItem.textContent = genre;
-      newSubList.appendChild(newSubItem);
-    }
+    // for(var genre of film.genres){
+    //   var newSubItem = document.createElement("li");
+    //   newSubItem.textContent = genre;
+    //   newSubList.appendChild(newSubItem);
+    // }
 
     newItem.setAttribute("class", "list__item");
     newImg.setAttribute("src", film.poster);
     newImg.setAttribute("class", "list__img");
     newText.setAttribute("class", "list__text");
-    newTime.setAttribute("datetime", "2022-03-12");
+    newBtn.setAttribute("class" , "list__bnt");
+    // newTime.setAttribute("datetime", "2022-03-12");D
+     var newBtnDataset = newBtn.dataset.filmid = film.id;
 
 
 
     newItem.appendChild(newImg);
     newItem.appendChild(newHeading);
     newItem.appendChild(newText);
-    newItem.appendChild(newTime);
-    newItem.appendChild(newSubList);
+    newItem.appendChild(newBtn);
+    // newItem.appendChild(newTime);
+    // newItem.appendChild(newSubList);
 
     element.appendChild(newItem);
 
@@ -69,14 +79,26 @@ function renderFilms(arr, element){
 }
 
 
-
+console.log();
 
 form.addEventListener("submit", evt =>{
   evt.preventDefault();
 
   const selectVal = select.value;
 
+
+  const inputVal = elInput.value
+
   let filterFilms = selectVal == "all" ? films : films.filter(element => element.genres.includes(selectVal))  ;
+
+
+  if(  films.genres ==  inputVal  ){
+
+    films.filter(element => element.genres.includes(inputVal))
+
+
+
+  }
 
   // if(selectVal === "all"){
   //   filterFilms = films;
@@ -89,7 +111,96 @@ form.addEventListener("submit", evt =>{
   renderFilms(filterFilms, elList);
 
 
-})
+});
+
+function newul(arr , element){
+ element.innerHTML= " "
+  arr.forEach(e =>{
+
+   var li = document.createElement("li");
+  //  var newsecondimg = document.createElement("img");
+  //  newsecondimg.setAttribute("scr" , e.poster)
+   
+
+    li.setAttribute("class" , "second-item");
+   
+    li.textContent = e.title ;
+
+  
+
+  var btn = document.createElement("button");
+  btn.setAttribute("class" , "btn-remove");
+  btn.textContent = "Remove";
+
+
+  li.appendChild(btn);
+
+  // li.appendChild(newsecondimg )
+
+   element.appendChild(li);
+  });
+
+  
+
+
+
+}
+
+
+
+elList.addEventListener("click" , evt =>{
+
+  const BtnDataset = evt.target.matches(".list__bnt") ;
+ 
+
+ if(BtnDataset){
+   const btnid = evt.target.dataset.filmid;
+
+
+   const btnfind = films.find( e => e.id === btnid );
+
+   NewBtnArray.push(btnfind);
+   console.log(NewBtnArray);
+
+ if(!NewBtnArray.includes(btnfind)){
+     NewBtnArray.push(btnfind)
+   
+ }
+
+  
+}
+
+
+
+
+
+newul(NewBtnArray , elSecondList)
+});
+
+elSecondList.addEventListener("click" , evt =>{
+
+  
+ 
+   const btnremove = evt.target.matches(".btn-remove");
+ 
+   if(btnremove){
+     const btniddd = evt.target.dataset.filmid;
+     const btnfindd = films.find( e => e.id === btniddd );
+     NewBtnArray.splice(btnfindd , 1);
+
+
+ 
+   }
+
+   
+
+newul(NewBtnArray , elSecondList)
+  });
+
+
+
+
+
 
 
 renderFilms(films, elList);
--- js/utils.js
+++ js/utils.js
@@ -12,3 +12,138 @@ function dateFormat(format){
 // console.log(dateFormat());
 
 
+
+
+// let a = "xumoyun";
+
+// {
+//   let a ="xumoyun"
+// }
+
+// console.log(a);
+
+
+
+// const people = [
+//   {
+//     name:"Ulug'bek",
+//     price:5000,
+//     debt:false,
+//   },
+
+//   {
+//     name:"Abdulaziz",
+//     price:10000,
+//     debt:false,
+//   },
+
+//   {
+//     name:"Mirjalol",
+//     price:100000,
+//     debt:true,
+//   },
+
+//   {
+//     name:"G'anisher",
+//     price:50000,
+//     debt:false,
+//   },
+
+//   {
+//     name:"Shahboz",
+//     price:70000,
+//     debt:false,
+//   },
+
+//   {
+//     name:"Humoyun",
+//     price:90000,
+//     debt:true,
+//   },
+// ]
+
+
+// const getaverage = (people) =>{
+
+// const add = people.reduce((qiymat, element) => {
+//   return qiymat + element
+//   },0)
+
+
+//   return add / people.length;
+
+// }
+
+// console.log(add);
+
+
+// const a = [4,5,8,2]
+
+// const b = [a].sort((a,b) => a-b ).map(d => d * 10);
+
+// console.log(a);
+// console.log(b);
+
+// let a = Array(100).fill(null).map(() => Math.floor(Math.random() * 2000) -1000 );
+
+// console.log(a);
+
+
+// const a = [-254, 578, -633, -421, 812, -235, 65, 814, -953, -449, -656, 415, 953, 613, 97, 444, -324, -898, -703, -979, -464, -484, 326, 866, 998, -287, -70, 393, 271, 821, -980, -723, -826, 237, -198, -748, 14, -490, 46, 638, -668, 674, -163, -607, -810, -244, -333, 34, -597, -548, 930, 316, 856, 332, 788, -787, -193, 455, 498, 746, -164, -958, 304, -534, 67, -644, 951, 758, -542, 109, -631, 334, 204, -796, 431, -922, -659, 319, 545, 360, -540, -263, -93, -876, -794, 461, -248, -165, 901, 924, -789, -289, -614, -187, 24, -591, -669, 822, -4, 431];
+
+
+// const b = [...a].sort((a,b) => a - b);
+
+// console.log(a);
+// console.log(b);
+
+
+
+
+// let a = [2,5,7,9,3,8,4,2,7,60,8]
+
+
+
+// for (let i = 0; i < a.length; i++) {
+
+//   if(a[i]<= 100){
+//     console.log("yes");
+//   }
+  
+  
+// }
+
+//   let b = a.some(b => b<32);
+
+// console.log(b );
+
+
+// let b = a.every(b => b < a.length);
+
+// console.log(b + " false");
+
+
+// const b = [...a].sort((a,b) => a - b);
+
+
+// console.log(a);
+// console.log(b);
+
+
+
+
+
+
+
+
+
+
+
+
+
+// for (let i = 0; i < .length; index++) {
+//   const element = array[index];
+  
+
+
+
